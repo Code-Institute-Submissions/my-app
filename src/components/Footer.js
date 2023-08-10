53eacadce54b319d3b41bbcc7bbe47450039df31
Imalsha0330 <samaranayake.imalsha@gmail.com>
2023-08-10 15:02:40 +0200

    Updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -26,13 +26,14 @@ const Footer = () => {
                 </div>
             </div>
             
-            <div className="right"></div>
-            <h4>About the Company</h4>
-            <p>I am Imalsha and I'm a Front End Developer & Founder of this Company. I have done various Frond End Projects for almost 20 years. I enjoy doing new projects and design challenges.</p>
-            <div className="social">
+            <div className="right">     
+                <h4>About the Company</h4>
+                <p>I am Imalsha and I'm a Front End Developer & Founder of this Company. I have done various Frond End Projects for almost 20 years. I enjoy doing new projects and design challenges.</p>
+                <div className="social">
                 <FaFacebook size={30} style={{color: "#fff", marginRight: "1rem" }}/>
                 <FaTwitter size={30} style={{color: "#fff", marginRight: "1rem" }}/>
                 <FaLinkedin size={30} style={{color: "#fff", marginRight: "1rem" }}/>
+                </div>
             </div>
         </div>
     </div>
