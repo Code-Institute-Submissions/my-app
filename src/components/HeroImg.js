a70ecf0800d4f0a1d6e391d7c131353a2d24cc0e
Imalsha0330 <samaranayake.imalsha@gmail.com>
2023-08-10 11:29:57 +0200

    Hero Image Added

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,12 +1,22 @@
 import "./HeroImgStyles.css";
 
 import React from 'react'
+import IntroImg from "../assets/introimg.jpg"
+import { Link } from "react-router-dom";
 
 const HeroImg = () => {
   return (
     <div className="hero">
         <div className="mask">
-            
+            <img className="into-img" src={IntroImg} alt="Intro Image"/>
+        </div>
+        <div className="content">
+            <p>HI, I'M a FREELANCER.</p>
+            <h1>Front End Developer</h1>
+            <div>
+                <Link to="/project" className="btn">Projects</Link>
+                <Link to="/contact" className="btn">Contact</Link>
+            </div>
         </div>
     </div>
   )
