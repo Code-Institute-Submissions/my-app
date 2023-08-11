28c9dc565297e77cd3d1c7c7a4f9a4d886db1ed2
Imalsha0330 <samaranayake.imalsha@gmail.com>
2023-08-11 12:23:47 +0200

    Updated

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,16 +1,15 @@
 import "./WorkCardStyles.css";
 
 import React from 'react';
-import pro1 from "../assets/Projectonebacke.jpg";
 import { NavLink } from "react-router-dom";
 
-const WorkCard = () => {
+const WorkCard = (props) => {
     return (
         <div className="project-card">
-            <img src={pro1} alt="Image"/>
-            <h2 className="project-title">Project Title</h2>
+            <img src={props.imgsrc} alt="Image"/>
+            <h2 className="project-title">{props.title}</h2>
             <div className="pro-details">
-                <p>This is a recent project what I have done. This is a top Backery in the USA. They wanted to sell their products online and wanted to increase their business more. With this website they managed to increase their anual business rate with 100% compare to previous years.</p>
+                <p>{props.text}</p>
                 <div className="pro-btns">
                     <NavLink to="url.com" className="btn">View</NavLink>
                     <NavLink to="url.com" className="btn">Source</NavLink>
---
+++ src/components/WorkCardData.js
@@ -0,0 +1,26 @@
+import pro1 from "../assets/Projectonebacke.jpg"
+import pro2 from "../assets/projecttwocarrent.jpg"
+import pro3 from "../assets/projectthreehotel.jpg"
+
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "Backery Website",
+        text: "This is a recent project what I have done. This is a top Backery in the USA. They wanted to sell their products online and wanted to increase their business more. With this website they managed to increase their anual business rate with 100% compare to previous years.",
+    },
+
+    {
+        imgsrc: pro2,
+        title: "Car Rental Service Website",
+        text: "This is a recent project what I have done. This is a top car rental service in the USA. They wanted to sell their products online and wanted to increase their business more. With this website they managed to increase their anual business rate with 100% compare to previous years.",
+    },
+
+    {
+        imgsrc: pro3,
+        title: "Hotel Website",
+        text: "This is a recent project what I have done. This is a top Hotel in the USA. They wanted to sell their products online and wanted to increase their business more. With this website they managed to increase their anual business rate with 100% compare to previous years.",
+    },
+];
+
+export default ProjectCardData;
+
