c0c2a29e4a1092de906329a9e24f06be4e52394f
Imalsha0330 <samaranayake.imalsha@gmail.com>
2023-08-11 11:51:50 +0200

    Code Updated and JS files added

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -5,24 +5,19 @@ import pro1 from "../assets/Projectonebacke.jpg";
 import { NavLink } from "react-router-dom";
 
 const WorkCard = () => {
-  return (
-    <div className="work-container">
-        <h1 className="project-heading">Projects</h1>
-        <div className="project-container">
-            <div className="project-card">
-                <img src={pro1} alt="Image"/>
-                <h2 className="project-title">Project Title</h2>
-                <div className="pro-details">
-                    <p>This is a recent project what I have done. This is a top Backery in the USA. They wanted to sell their products online and wanted to increase their business more. With this website they managed to increase their anual business rate with 100% compare to previous years.</p>
-                    <div className="pro-btns">
-                        <NavLink to="url.com" className="btn">View</NavLink>
-                        <NavLink to="url.com" className="btn">Source</NavLink>
-                    </div>
+    return (
+        <div className="project-card">
+            <img src={pro1} alt="Image"/>
+            <h2 className="project-title">Project Title</h2>
+            <div className="pro-details">
+                <p>This is a recent project what I have done. This is a top Backery in the USA. They wanted to sell their products online and wanted to increase their business more. With this website they managed to increase their anual business rate with 100% compare to previous years.</p>
+                <div className="pro-btns">
+                    <NavLink to="url.com" className="btn">View</NavLink>
+                    <NavLink to="url.com" className="btn">Source</NavLink>
                 </div>
             </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
 export default WorkCard;
