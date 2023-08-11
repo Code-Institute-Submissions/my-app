05365785b5ec9337ecdaabe94a029ebf70ab2a28
Imalsha0330 <samaranayake.imalsha@gmail.com>
2023-08-11 16:44:55 +0200

    Description and Images Added to about page

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,11 +1,32 @@
 import "./AboutContentStyles.css";
 
 import React from 'react'
+import { Link } from "react-router-dom";
+import image1 from "../assets/aboutone.jpg"
+import image2 from "../assets/abouttwo.jpg"
 
 const AboutContent = () => {
   return (
-    <div>AboutContent</div>
+    <div className="about">
+        <div className="left">
+            <h1>Who Am I?</h1>
+            <p>I'm a Front-End-Developer. I create responsive secure websites for my clients.</p>
+            <Link to="/contact">
+                <button className="btn">Contact</button>
+            </Link>
+        </div>
+        <div className="right">
+            <div className="img-container">
+                <div className="img-stack top">
+                    <img src={image1} className="img" alt="Image"/>
+                </div>
+                <div className="img-stack bottom">
+                    <img src={image2} className="img" alt="Image"/>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
-export default AboutContent
+export default AboutContent;
